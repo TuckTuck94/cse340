8f91092dfc90b4b9c95ba927b7a895a9a2767064
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2024-01-27 21:31:10 -0600

    fixed the 404 error

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use("/inv", inventoryRoute)
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
-
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
@@ -42,10 +41,9 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
-  res.render("errors/error", {
+  res.render("errors/errors", {
     title: err.status || 'Server Error',
-    message,
+    message: err.message,
     nav
   })
 })
