3d2e7f943f8855e23eae4878e380156b214c5654
TuckTuck94 <114019356+TuckTuck94@users.noreply.github.com>
2024-01-27 18:00:28 -0600

    w03 learning activities

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -25,21 +25,6 @@ async function getInventoryByClassificationId(classification_id) {
   }
 }
 
-/* ***************************
- *  Get all details for an item by inventory_id
- * ************************** */
-async function getDetailsByInventoryId(inventory_id) {
-  try {
-    const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
-      WHERE i.inv_id = $1`,
-      [inventory_id]
-    )
-    return data.rows
-  } catch (error) {
-    console.error("getdetailsbyinventoryid error " + error)
-  }
-}
+module.exports = {getClassifications}
 
-
-module.exports = {getClassifications, getInventoryByClassificationId, getDetailsByInventoryId};
+module.exports = {getClassifications, getInventoryByClassificationId};
--- server.js
+++ server.js
@@ -1,41 +1,35 @@
 /* ******************************************
  * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-
+ * application. It is used to control the project. 
+*******************************************/
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require('./utilities/');
+const inventoryRoute = require("./routes/inventoryRoute.js")
+const utilities = require("./utilities/index.js")
+
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at vies root
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
-// app.get("/", function(req, res){
-//   res.render("index", {title: "Home"})
-// })
-
 // Inventory routes
 app.use("/inv", inventoryRoute)
-
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
